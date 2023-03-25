d3f0e820399d5f609701b90643b6cd39d0a1b781
Myp3uk1234 <114804515+Myp3uk1234@users.noreply.github.com>
2023-03-25 13:41:31 +0200

    start project

---
+++ index.js
@@ -0,0 +1,72 @@
+// $(document).ready(function () {
+//     fixedHeader()
+//     $(window).scroll(function () {
+//         fixedHeader()
+//     });
+//     function fixedHeader() {
+//         if (window.pageYOffset > 0) {
+//             $("header").addClass("fixed")
+//         } else {
+//             $("header").removeClass("fixed")
+//         }
+//     }
+// });
+// let arr = ['red', 'yellow', 'white']
+// arr.pop()
+// arr.push('green')
+// console.log(arr)
+
+
+// let string = 'hello'
+// console.log(string[1])
+
+
+// let toString = arr.toString()
+// console.log(toString)
+
+
+// let arrToString = arr.join(' ')
+// // console.log(arrToString)
+
+
+// let arrArr = arr.join(' and ')
+// let arrtoString = arr.join(' and ')
+// console.log( arr[0] , arr[1] , 'and' , arr[2] )
+// let mtr = []
+// mtr[0] = 'a'
+// mtr[1] = 'b'
+
+
+// console.log(mtr)
+
+// let obj = {a:14, b:95}
+// console.log(obj['a'] + obj['b'])
+
+// let week = {
+//     1: "monday",
+//     2: "tuesday",
+//     3: "wednesday",
+//     4: "thursday",
+//     5: "friday",
+//     6: "saturday",
+//     7: "sunday",
+//   };
+//   console.log (week('6'))
+
+let arr =  ['bannan','orange','apple']
+  arr[1]='kiwi'
+  console.log(arr)
+
+  let arrsrt = arr.join(' ')
+
+  console.log(arrsrt)
+
+  let str = 'Hello World'
+  let newstr = str.toUpperCase()
+  console.log(newstr)
+
+let cont = str.length
+console.log(cont)
+
+    let contarr = arr.length
+    console.log(contarr)
